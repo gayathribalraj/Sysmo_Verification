f68a5d8be85deb9d0101ac531c6e63a3a13cfcd2
gayathribalraj <gayathri.balraj@sysarcinfomatix.com>
2026-02-06 12:55:55 +0530

    fix:*As per FSD first 8 digit should be masked while onboarding the *customer through Aadhar If the user enter wrong OTP Aadhar validation time no alert is showing

--- lib/src/widget/uiwidgetprops/kyc_input_field.dart
+++ lib/src/widget/uiwidgetprops/kyc_input_field.dart
@@ -6,6 +6,34 @@
 
 import 'package:sysmo_verification/kyc_validation.dart';
 
+/// Custom TextInputFormatter for Aadhaar masking
+/// Masks first 8 digits and shows only last 4 digits
+class AadhaarMaskFormatter extends TextInputFormatter {
+  final int visibleDigits;
+  final String maskChar;
+
+  AadhaarMaskFormatter({this.visibleDigits = 4, this.maskChar = '*'});
+
+  @override
+  TextEditingValue formatEditUpdate(
+    TextEditingValue oldValue,
+    TextEditingValue newValue,
+  ) {
+    // Allow only digits
+    final digitsOnly = newValue.text.replaceAll(RegExp(r'[^0-9]'), '');
+
+    // Limit to 12 digits for Aadhaar
+    final limitedDigits = digitsOnly.length > 12
+        ? digitsOnly.substring(0, 12)
+        : digitsOnly;
+
+    return TextEditingValue(
+      text: limitedDigits,
+      selection: TextSelection.collapsed(offset: limitedDigits.length),
+    );
+  }
+}
+
 /// KYC input field widget with reactive form integration
 class KYCInputField extends StatefulWidget {
   final FormProps formProps;
@@ -17,6 +45,7 @@ class KYCInputField extends StatefulWidget {
   final ReactiveFormFieldCallback<String>? onChange;
   final RegExp? validationPattern;
   final bool obscureText;
+  final bool maskAadhaar;
 
   const KYCInputField({
     super.key,
@@ -29,6 +58,7 @@ class KYCInputField extends StatefulWidget {
     this.onChange,
     this.validationPattern,
     this.obscureText = false,
+    this.maskAadhaar = false,
   });
 
   @override
@@ -37,11 +67,19 @@ class KYCInputField extends StatefulWidget {
 
 class _KYCInputFieldState extends State<KYCInputField> {
   late bool _isObscured;
+  final TextEditingController _maskedController = TextEditingController();
+  bool _isFocused = false;
 
   @override
   void initState() {
     super.initState();
     _isObscured = widget.obscureText;
+  }
+
+  @override
+  void dispose() {
+    _maskedController.dispose();
+    super.dispose();
   }
 
   void _toggleObscure() {
@@ -61,58 +99,9 @@ class _KYCInputFieldState extends State<KYCInputField> {
         children: [
           IgnorePointer(
             ignoring: widget.disabled,
-            child: ReactiveTextField<String>(
-              autofocus: false,
-              keyboardType: widget.keyboardType,
-              formControlName: widget.formProps.formControlName,
-              onChanged: widget.onChange,
-              maxLength: widget.formProps.maxLength,
-              obscureText: _isObscured,
-              style:
-                  widget.styleProps.textStyle ?? const TextStyle(fontSize: 14),
-              decoration:
-                  widget.styleProps.inputDecoration ??
-                  InputDecoration(
-                    label: RichText(
-                      text: TextSpan(
-                        text: widget.formProps.label,
-                        style:
-                            widget.styleProps.textStyle ??
-                            const TextStyle(color: Colors.black, fontSize: 18),
-                        children: [
-                          TextSpan(
-                            text: widget.formProps.mandatory ? ' *' : '',
-                            style: const TextStyle(color: Colors.red),
-                          ),
-                        ],
-                      ),
-                    ),
-                    errorStyle: const TextStyle(
-                      color: Colors.red,
-                      fontSize: 12,
-                    ),
-                    suffixIcon: widget.obscureText
-                        ? IconButton(
-                            icon: Icon(
-                              _isObscured
-                                  ? Icons.visibility_off
-                                  : Icons.visibility,
-                              color: Colors.grey,
-                            ),
-                            onPressed: _toggleObscure,
-                          )
-                        : null,
-                  ),
-              validationMessages: widget.formProps.validator != null
-                  ? {
-                      '': (control) {
-                        final abstractControl =
-                            control as AbstractControl<dynamic>;
-                        return widget.formProps.validator!(abstractControl);
-                      },
-                    }
-                  : null,
-            ),
+            child: widget.maskAadhaar
+                ? _buildMaskedAadhaarField()
+                : _buildRegularTextField(),
           ),
           ReactiveFormConsumer(
             builder: (context, form, child) {
@@ -139,5 +128,191 @@ class _KYCInputFieldState extends State<KYCInputField> {
         ],
       ),
     );
+  }
+
+  /// Builds the regular text field without masking
+  Widget _buildRegularTextField() {
+    return ReactiveTextField<String>(
+      autofocus: false,
+      keyboardType: widget.keyboardType,
+      formControlName: widget.formProps.formControlName,
+      onChanged: widget.onChange,
+      maxLength: widget.formProps.maxLength,
+      obscureText: _isObscured,
+      style: widget.styleProps.textStyle ?? const TextStyle(fontSize: 14),
+      decoration:
+          widget.styleProps.inputDecoration ??
+          InputDecoration(
+            label: RichText(
+              text: TextSpan(
+                text: widget.formProps.label,
+                style:
+                    widget.styleProps.textStyle ??
+                    const TextStyle(color: Colors.black, fontSize: 18),
+                children: [
+                  TextSpan(
+                    text: widget.formProps.mandatory ? ' *' : '',
+                    style: const TextStyle(color: Colors.red),
+                  ),
+                ],
+              ),
+            ),
+            errorStyle: const TextStyle(color: Colors.red, fontSize: 12),
+            suffixIcon: widget.obscureText
+                ? IconButton(
+                    icon: Icon(
+                      _isObscured ? Icons.visibility_off : Icons.visibility,
+                      color: Colors.grey,
+                    ),
+                    onPressed: _toggleObscure,
+                  )
+                : null,
+          ),
+      validationMessages: widget.formProps.validator != null
+          ? {
+              '': (control) {
+                final abstractControl = control as AbstractControl<dynamic>;
+                return widget.formProps.validator!(abstractControl);
+              },
+            }
+          : null,
+    );
+  }
+
+  /// Builds masked Aadhaar text field
+  /// Shows first 8 digits as * and last 4 digits visible when not focused
+  Widget _buildMaskedAadhaarField() {
+    return ReactiveFormConsumer(
+      builder: (context, form, child) {
+        final control = form.control(widget.formProps.formControlName);
+        final actualValue = control.value ?? '';
+        final displayValue = _isFocused
+            ? actualValue
+            : _getMaskedAadhaar(actualValue);
+
+        return GestureDetector(
+          onTap: () {
+            setState(() {
+              _isFocused = true;
+            });
+          },
+          child: Stack(
+            children: [
+              // Actual input field (visible when focused/editing)
+              Opacity(
+                opacity: _isFocused ? 1.0 : 0.0,
+                child: Focus(
+                  onFocusChange: (hasFocus) {
+                    if (!hasFocus) {
+                      setState(() {
+                        _isFocused = false;
+                      });
+                    }
+                  },
+                  child: ReactiveTextField<String>(
+                    autofocus: false,
+                    keyboardType: TextInputType.number,
+                    formControlName: widget.formProps.formControlName,
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly,
+                      LengthLimitingTextInputFormatter(12),
+                    ],
+                    onChanged: (ctrl) {
+                      widget.onChange?.call(ctrl);
+                    },
+                    maxLength: widget.formProps.maxLength,
+                    style:
+                        widget.styleProps.textStyle ??
+                        const TextStyle(fontSize: 14),
+                    decoration:
+                        widget.styleProps.inputDecoration ??
+                        InputDecoration(
+                          label: RichText(
+                            text: TextSpan(
+                              text: widget.formProps.label,
+                              style:
+                                  widget.styleProps.textStyle ??
+                                  const TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 18,
+                                  ),
+                              children: [
+                                TextSpan(
+                                  text: widget.formProps.mandatory ? ' *' : '',
+                                  style: const TextStyle(color: Colors.red),
+                                ),
+                              ],
+                            ),
+                          ),
+                          errorStyle: const TextStyle(
+                            color: Colors.red,
+                            fontSize: 12,
+                          ),
+                        ),
+                    validationMessages: widget.formProps.validator != null
+                        ? {
+                            '': (control) {
+                              final abstractControl =
+                                  control as AbstractControl<dynamic>;
+                              return widget.formProps.validator!(
+                                abstractControl,
+                              );
+                            },
+                          }
+                        : null,
+                  ),
+                ),
+              ),
+              // Masked display field (visible when not focused)
+              if (!_isFocused)
+                IgnorePointer(
+                  child: TextField(
+                    controller: TextEditingController(text: displayValue),
+                    readOnly: true,
+                    style:
+                        widget.styleProps.textStyle ??
+                        const TextStyle(fontSize: 14),
+                    decoration:
+                        widget.styleProps.inputDecoration ??
+                        InputDecoration(
+                          label: RichText(
+                            text: TextSpan(
+                              text: widget.formProps.label,
+                              style:
+                                  widget.styleProps.textStyle ??
+                                  const TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 18,
+                                  ),
+                              children: [
+                                TextSpan(
+                                  text: widget.formProps.mandatory ? ' *' : '',
+                                  style: const TextStyle(color: Colors.red),
+                                ),
+                              ],
+                            ),
+                          ),
+                          errorStyle: const TextStyle(
+                            color: Colors.red,
+                            fontSize: 12,
+                          ),
+                        ),
+                  ),
+                ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  /// Returns masked Aadhaar number - first digits as *, last 4 visible
+  String _getMaskedAadhaar(String value) {
+    if (value.isEmpty) return '';
+    if (value.length <= 4) return value;
+
+    final visiblePart = value.substring(value.length - 4);
+    final maskedLength = value.length - 4;
+    return '${'*' * maskedLength}$visiblePart';
   }
 }
